64759815f33fdc802659621cfcd941b18fd228c3
Riccardo Murri <riccardo.murri@gmail.com>
2011-08-20 11:46:30 +0000

    AUX-TeX "auto/*.el" files should not be versioned.
    removed:
      auto/ainfty.el
      auto/arrows.el
      auto/btc.el
      auto/csref.el
      auto/fd.el
      auto/figures.el
      auto/gc.el
      auto/graphcomplex.el
      auto/index.el
      auto/kontsevich.el
      auto/math.el
      auto/miscmath.el
      auto/prelim.el
      auto/prova.el
      auto/rt.el
    
    
    git-svn-id: https://fatghol.googlecode.com/svn/trunk@242 e2c0af65-7b97-2394-be7d-76dd08efcea0

--- auto/csref.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook "csref"
- (function
-  (lambda ()
-    (TeX-add-symbols
-     '("renewcsref" 1)
-     '("newcsref" 1)
-     "label"
-     "refsectionname"))))
-
--- auto/prova.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook "prova"
- (lambda ()
-    (TeX-run-style-hooks
-     "csref"
-     "latex2e"
-     "art10"
-     "article"
-     "a4paper")))
-
