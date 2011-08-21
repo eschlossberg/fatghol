11ba530c1ae9effcd7bedc491118953f86e60f96
Riccardo Murri <riccardo.murri@gmail.com>
2011-08-21 17:11:21 +0000

    mgn.sh: Try to use GNU time to gather execution stats.
    git-svn-id: https://fatghol.googlecode.com/svn/trunk@536 e2c0af65-7b97-2394-be7d-76dd08efcea0

--- mgn.sh
+++ mgn.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+## set up Python module search path
 basedir="`dirname $0`"
 
 arch="`uname -m`"
@@ -9,5 +10,25 @@ os="`uname -s | tr A-Z a-z`"
 PYTHONPATH="$basedir":"$basedir"/build/lib.${os}-${arch}-${v}:$PYTHONPATH; export PYTHONPATH
 LD_LIBRARY_PATH="$basedir"/build/lib.${os}-${arch}-${v}:"$basedir"/sw/lib:$LD_LIBRARY_PATH; export LD_LIBRARY_PATH
 
-#exec python -O /usr/lib/python2.5/cProfile.py -o profile.bin "$basedir"/main.py "$@"
-exec python "$basedir"/main.py "$@"
+## try to use GNU time
+if [ -x /usr/bin/time ]; then
+    # usueal location in Linux systems
+    time_cmd=/usr/bin/time
+else
+    # whatever is in PATH
+    time_cmd=time
+fi
+if ($time_cmd --format='%M' echo) >/dev/null 2>&1; then
+    :   # time is GNU time, nothing to do
+else
+    time_cmd=''
+fi
+
+## run the main program, with timing information
+if [ -n "$time_cmd" ]; then
+    exec $time_cmd --format='DEBUG: wctime=%e cputime(usr)=%U cputime(sys)=%S maxmem=%MkB cs(involuntary)=%c cs(voluntary)=%w majflt=%F minflt=%R' python "$basedir"/main.py "$@"
+else
+    #exec python -O /usr/lib/python2.5/cProfile.py -o profile.bin "$basedir"/main.py "$@"
+    exec  python "$basedir"/main.py "$@"
+fi
+
