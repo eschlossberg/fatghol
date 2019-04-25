a6dca5fd14207176d3fd2817119c99d304e6e5fd
Eli Schlossberg <Schlossberg2@wisc.edu>
2019-04-25 10:50:08 -0500

    bugfixes

--- fatghol/test.py
+++ fatghol/test.py
@@ -9,9 +9,9 @@ to_compute = [(2,2)]
 for (g,n) in to_compute:
     mgn = homology_reps.NullSpaceComplex(g,n)
     f.write("g:{} n:{}\n".format(g, n))
-    print("g:", g, "n:", n)
+    #print("g:", g, "n:", n)
     for i in range(len(mgn)):
         f.write("Degree {}:{}\n".format(i, mgn.homology_characters[i]))
-        print(i, mgn.homology_characters[i])
+        #print(i, mgn.homology_characters[i])
     f.write("\n\n")
-
+f.close()
